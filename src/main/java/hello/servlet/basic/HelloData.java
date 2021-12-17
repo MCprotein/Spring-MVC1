faaef4901ce22efca827619c94b898c02cc990f2
MCprotein <chss3339@gmail.com>
2021-12-18 01:54:28 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+    //Json 파싱할 수 있는 객체
+
+
+}
