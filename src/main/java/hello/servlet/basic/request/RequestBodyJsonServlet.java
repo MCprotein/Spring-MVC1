2559cfdd24e5e0be8dfa465dffe48c958261c28b
MCprotein <chss3339@gmail.com>
2021-12-20 01:32:59 +0900

    HttpServletResponse - 기본사용법_content,cookies,redirect

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -25,7 +25,7 @@ public class RequestBodyJsonServlet extends HttpServlet {
 
         System.out.println("messageBody = " + messageBody);
 
-        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class); //Json을 읽는 기능
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class); //Json을 읽는 기체
 
         System.out.println("helloData.username = " + helloData.getUsername());
         System.out.println("helloData.age = " + helloData.getAge());
