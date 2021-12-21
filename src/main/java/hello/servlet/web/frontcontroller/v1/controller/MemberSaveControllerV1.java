ef0521b0dfe656b5bd1ebf24ce97cfc4921f46e5
MCprotein <chss3339@gmail.com>
2021-12-21 23:05:25 +0900

    프론트 컨트롤러 도입-v1

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,34 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age")); //파라미터 받고
+
+        Member member = new Member(username, age);
+        memberRepository.save(member); //비즈니스 로직 호출하고
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member); //모델에 데이터 담고
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response); //뷰로 던지고
+
+    }
+}
